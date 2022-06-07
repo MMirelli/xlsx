67a6425598adec0cd930db028137471aa7386ffd
Massimiliano Mirelli <massimilianomirelli.mm@gmail.com>
2022-06-07 14:56:59 +0300

    Add support for matching cell containing regex

--- row.go
+++ row.go
@@ -29,13 +29,26 @@ func (r *Row) AddCell() *Cell {
 	return cell
 }
 
-// Contains checks whether a given regex is contained in a xlsx.Cell of the given
-// xlsx.Row. It returns its index if found, otherwise -1.
+// Contains(regex string) - checks whether a given regex is contained in a xlsx.Cell of the given
+// xlsx.Row. It returns true the regex matches a cell in the given row.
 func (r *Row) Contains(regex string) bool{
 	var rst bool = false
 	for _, cell := range r.Cells{
 		if found, _ := regexp.Match(regex, []byte(cell.Value)); found{
 			rst = true
+			break
+		}
+	}
+	return rst
+}
+
+// IdxMatchedCell(regex string) - whether a given regex is contained in a xlsx.Cell of the given
+// xlsx.Row. It returns its index if found, otherwise -1.
+func (r *Row) IdxMatchedCell(regex string) int{
+	var rst int = -1
+	for idx, cell := range r.Cells{
+		if found, _ := regexp.Match(regex, []byte(cell.Value)); found{
+			rst = idx
 			break
 		}
 	}
